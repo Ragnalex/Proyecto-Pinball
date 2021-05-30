a485614de104cfc019a46779f7e980adf37e7ab4
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2021-05-30 19:35:53 -0400

    Puntuación

---
+++ Space Cadets Fruna/Assets/Cowboy Lvel/ScoreController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+
+
+    public int score;
+    public Text TXTscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TXTscore.text = "" + score;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Bumper25"){
+            score = score + 25;
+        }
+        else if (collision.gameObject.tag == "Bumper50"){
+            score = score + 50;
+        }
+        else if (collision.gameObject.tag == "Bumper100"){
+            score = score + 100;
+        }
+    }
+}
