6a937fd8f84495f19ef82461428e452d20c9de1a
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2021-07-03 01:33:37 -0400

    Arreglo plunger animacion

--- Space Cadets Fruna/Assets/Test.cs
+++ Space Cadets Fruna/Assets/Test.cs
@@ -7,54 +7,76 @@ public class Test : MonoBehaviour
 {
     float power;
     float minPower = 0f;
-    public float maxPower = 100f;
+    public float maxPower = 2000;
     public Slider PowerSlider;
     List<Rigidbody2D> ballList;
     bool ballReady;
+    public GameObject plunger;
+    private float count;
+    private Vector3 position;
+    private bool move;
 
     void Start()
     {
         PowerSlider.minValue = 0f;
         PowerSlider.maxValue = maxPower;
         ballList = new List<Rigidbody2D>();
+        count = 0.5f;
+        move = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PowerSlider.value = power;
         if (ballReady){
             PowerSlider.gameObject.SetActive(true);
         }
         else{
             PowerSlider.gameObject.SetActive(false);
         }
-        PowerSlider.value = power;
         if (ballList.Count > 0){
             ballReady = true;
-            if (Input.GetKeyDown(KeyCode.Space)){
-                if (power <= maxPower){
-                    power += 50*Time.deltaTime;
+            if (Input.GetKey(KeyCode.Space)){
+                if (power <= 2000){
+                    float aux = 800*Time.deltaTime;
+                    power = power + aux;
+                    count = count + 0.5f;
+                    plunger.transform.Translate(Vector3.down * Time.deltaTime);
                 }
+
             }
             if (Input.GetKeyUp(KeyCode.Space)){
                 foreach(Rigidbody2D r in ballList){
-                    r.AddForce(power*Vector3.forward);
+                    r.AddForce(power*Vector3.up);
                 }
+                move = true;
             }
         }
         else{
             ballReady = false;
             power = 0f;
         }
-    }
-
-    private void OnTriggerEnter(Collider other){
-        if (other.gameObject.CompareTag("Ball")){
-            ballList.Add(other.gameObject.GetComponent<Rigidbody2D>());
+        if (move == true){
+            if (plunger.transform.position.y <= position.y){
+                plunger.transform.Translate(Vector3.up*Time.deltaTime);
+            }
+            else{
+               move = false;
+            }
+                    
         }
     }
 
-    private void OnTriggerExit(Collider other){
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Ball")){
+            ballList.Add(other.gameObject.GetComponent<Rigidbody2D>());
+            position = plunger.transform.position;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other){
         if (other.gameObject.CompareTag("Ball")){
             ballList.Remove(other.gameObject.GetComponent<Rigidbody2D>());
             power = 0f;
