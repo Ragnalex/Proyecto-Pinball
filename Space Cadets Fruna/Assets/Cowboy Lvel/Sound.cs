34a625ae79446ef0b8fc0d0a0628065a6ff477da
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2021-07-08 18:06:32 -0400

    Sonido bumper añadido

---
+++ Space Cadets Fruna/Assets/Cowboy Lvel/Sound.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour
+{
+    
+
+    public AudioClip colision;
+
+    public float volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ball")
+        {
+            AudioSource.PlayClipAtPoint(colision, Camera.main.transform.position, volume);
+        }
+        {
+            
+        }
+    }
+}
